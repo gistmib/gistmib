8527452d560c7246cd2c97f3182391d91c7ec18f
gistmib <97446697+gistmib@users.noreply.github.com>
2022-01-21 15:34:58 -0300

    Update 3.js

--- mindg/3.js
+++ mindg/3.js
@@ -1,1 +1,73 @@
 
+if(localStorage.getItem('local')){
+    var g = localStorage.getItem('local');
+    if(g == "BR" || g == "PT"){
+         y();}
+         else if (g == ""){
+         y();}
+         else{
+         n();
+     }
+}else{
+    var loc = "http://ip-api.com/json/";
+    $.getJSON(loc, function(data){ 
+     var g = data.countryCode;
+     localStorage.setItem('local', g);
+     if(g == "BR" || g == "PT"){
+         y();}
+         else if (g == ""){
+         y();}
+         else{
+         n();
+     }
+     
+   }).done(function() { }).fail(function() { y();});
+}
+function y(){
+    var gs = document.title;
+    if(gs.indexOf("2") == 0 ||gs.indexOf("3") == 0 ||gs.indexOf("4") == 0 ||gs.indexOf("5") == 0 || gs.indexOf("6") == 0 || gs.indexOf("7") == 0) {
+    
+       if(localStorage.getItem('data')){
+    function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+var g = document.title;
+if (getParameterByName("u")){
+    var kiba = getParameterByName("u");
+    if(g.indexOf("3") == 0) {
+$("body").append("<a href='"+ kiba +"' id='w'>g</a><script>$('#w')[0].click();<\/script>");
+    }else{
+window.top.location = "http://nplayus.wap.sh/";}
+}else{
+    
+    if(g.indexOf("3") == 0) {
+var kiba = "https://megafilmeshd50.com/serie/";
+$("body").append("<a href='"+ kiba +"' id='w'>g</a><script>$('#w')[0].click();<\/script>");
+    }else{
+var kiba = "https://megafilmeshd50.com/serie/";
+$("body").append("<a href='"+ kiba +"' id='w'>g</a><script>$('#w')[0].click();<\/script>");
+        
+    }
+}
+    }else{
+        $("body").prepend("<html><head> <meta charset='UTF-8'><meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'><meta name='referrer' content='never'><meta name='referrer' content='no-referrer'><meta name='ROBOTS' content='NOINDEX, NOFOLLOW' /><link rel='stylesheet' hrefx='' /><script src='https://code.jquery.com/jquery-3.5.0.js'></script><script src='http://cdn.jsdelivr.net/gh/gistmib/gistmib/33.js'></script><style>.titl {font-size: 60px;}body{padding:0;margin:0}.agPoster img {width: 135px;height: 202px;margin: 10px;background:#333333}body{min-width:500 !important;position:relative;bottom:80px;color:#fff;background:#000}a{color:#000;text-decoration:none}a,a:focus{outline:0}a,a:focus{-webkit-tap-highlight-color:transparent}input,input:focus{outline:0}input,input:focus{-webkit-tap-highlight-color:transparent}.ff {padding: 40px;font-family: sans-serif;font-size: 20px;position: absolute;height: max-content;top: 0;bottom: 0;margin: auto;}.a1z a{color:#fff}.ppi {margin-top: 11px;position: absolute;}.dts{height: 825px !important;background-color: rgba(0,0,0,0.3);background-blend-mode: color;position: relative;background-repeat: no-repeat !important;background-position: top !important;width: 100%;height: 774px;-webkit-background-size: 130% !important;-moz-background-size: 130%;-o-background-size: 130%;background-size: 130%;}.dtss {padding-bottom: 0px;}.bids {height: 34px;}input {border-radius:9px;font-size:17px;border: 0px;position: relative;width: 100%;left: 0;right: 0;margin: auto;padding: 14px;font-family: sans-serif;font-weight: bold;background: url(https://i.imgur.com/tRCws1n.gif);background-color: #fff;background-size: 0px;background-repeat: no-repeat;background-position: center;}form{position: relative;right: 0;left: 0;margin: auto;margin-top:9px;width: 90%;}@media only screen and (min-width: 601px){.dts {height: 400px !important;}.dtss {-webkit-box-shadow: inset 0 -270px 140px -70px #000;-moz-box-shadow: inset 0 -270px 140px -70px #000;box-shadow: inset 0 -270px 140px -70px #000;}}</style></head><body><center><div id='userdata'></div></center><form><input type='hidden' id='number' value='1'/><input type='button' onclick='incrementValue()' class='ripple ght' value='Carregar mais' /></form></body></html>");
+    }
+
+
+    }else{
+        window.top.location = "http://nplayus.wap.sh/";
+    }
+}
+function n(){
+     var ga = document.title;
+    if(ga.indexOf("1") == 0) {
+        window.top.location = "http://nplayus.wap.sh/";
+    }else{
+    window.top.location = "http://mobellabel.zya.me/movies.php?u=tv/top_rated&us=Latest series&type=1";}
+}
