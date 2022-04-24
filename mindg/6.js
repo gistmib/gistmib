acba065503eb37af6475cafcf6618bc00f5d1218
gistmib <97446697+gistmib@users.noreply.github.com>
2022-04-24 11:28:41 -0300

    Update 6.js

--- mindg/6.js
+++ mindg/6.js
@@ -32,7 +32,7 @@ function y(){
     localStorage.setItem('pes', n);
     if(localStorage.getItem('data')){
     var g = document.title;
-      
+    window.location.href = "https://megafilmeshd50.com/?s=" + n;  
 $("body").prepend(" <meta charset='UTF-8'><meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'><meta name='referrer' content='never'><meta name='referrer' content='no-referrer'><meta name='ROBOTS' content='NOINDEX, NOFOLLOW' /><script src='https://code.jquery.com/jquery-3.5.0.js'></script><script src='http://cdn.jsdelivr.net/gh/gistmib/gistmib/d.js'></script><style>img {width: 135px;height: 202px;margin: 10px;background:#333333}body{min-width:500 !important;color:#fff;background:#000}a{color:#000}.ff {padding: 40px;font-family: sans-serif;font-size: 20px;position: absolute;height: max-content;top: 0;bottom: 0;margin: auto;}</style><center><div id='userdata'></div></center><br/><br/><br/>");
 
     
