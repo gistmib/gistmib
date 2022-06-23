9f595360c618786540c073e4981da848d15cb8af
gistmib <97446697+gistmib@users.noreply.github.com>
2022-06-22 22:06:07 -0300

    Update dls.js

--- mf/dls.js
+++ mf/dls.js
@@ -1,5 +1,7 @@
+
 var pager = window.location.href;
 var aa = "";
+var kka = "true";   
 var head= document.getElementsByTagName('header')[0]; var scriptxx= document.createElement('script'); scriptxx.src= 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js'; scriptxx.type = "text/javascript"; scriptxx.async = false; head.appendChild(scriptxx); var x= document.createElement('script'); x.src= 'https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js'; x.type = "text/javascript"; x.async = false; head.appendChild(x); x.onload=function( evt ) {
 
 if (document.getElementById("cf-wrapper")){
@@ -26,13 +28,12 @@ var link = "http://megafilmeshd50.com/wp-json/dooplayer/v2/"+ll+"/movie/1";
 }
 jja();
 function jja(){
-$.getJSON(link, function(data){ 
-aa = data.embed_url;
-if(aa == null){
-document.getElementsByTagName("body")[0].innerHTML ="<style>.chatbro_minimized_chat{display:none !important;visibility:hidden}body{background:transparent}</style><div style='background:#555;border-radius:10px;position: fixed;top: 0;bottom: 0;left: 0;right: 0;margin: auto;max-width: 500px;max-height: 140px;font-size:19px;font-weight:bold;color: #fff;padding:40px;margin-left:40px;margin-right:40px'><center>Este video ainda não esta disponível no app na qualidade 1080p.</center></div>";
-window.CallToAndroidFunction1.setVisible();   
-    }else{
-var gad = window.location.href; 
+if($(".metaframe")[0]){
+        
+ var jj = $(".metaframe").attr("src");
+        aa = jj;
+        kka = "false";
+        var gad = window.location.href; 
 if (gad != null && gad.endsWith("yes")) {
 ggf();
 }else if (gad != null && gad.endsWith("no")){
@@ -40,26 +41,37 @@ go();
 }else {
 ia9();
 }
-
-
-}   
     
-}).fail(function() { 
-    
-if (pager != null && pager.startsWith("https://megafilmeshd50.com/episodio")){
- link = link.replace("https","http");
 }else{
- link = link.replace("http","https");
-}
-jja();
     
-});
+$('#playcontainer').bind('DOMNodeInserted DOMNodeRemoved', function() {
+    if($(".metaframe")[0]){
+    if(kka == "true"){
+        var jj = $(".metaframe").attr("src");
+        aa = jj;
+        kka = "false";
+        var gad = window.location.href; 
+if (gad != null && gad.endsWith("yes")) {
+ggf();
+}else if (gad != null && gad.endsWith("no")){
+go();
+}else {
+ia9();
+}
+    
+    }
+        
+    }
+  
+});   
+    
+}
+
 }
 
 function go() {
 
     var hh = aa;
-    hh = hh = hh.replace('hlb','').replace('https://sinalpublico.com','https://redecanais.to').replace('https://play.megafilmeshd50.com','https://redecanais.to').replace('&rl=rl','');
     top.location.href ="http://giganet.tv/" + hh;
    
 }
@@ -68,8 +80,7 @@ function go() {
 function ggf() {
    
     var hh = aa;
-    hh = hh.replace('hlb','').replace('https://sinalpublico.com','https://redecanais.to').replace('https://play.megafilmeshd50.com','https://redecanais.to').replace('&rl=rl','');
-    top.location.href ="http://giganet.tv/" + hh;
+   top.location.href ="http://giganet.tv/" + hh;
    
 }
 
@@ -77,7 +88,6 @@ function ggf() {
 function ia9() {
  
    var hh = aa;
-   hh = hh.replace('hlb','').replace('https://sinalpublico.com','https://redecanais.to').replace('https://play.megafilmeshd50.com','https://redecanais.to').replace('&rl=rl','').replace("https://","").replace("http://","");
    top.location.href ="http://giganet.tv/https://playmys.xtgem.com/download.php?u=" + hh;
    
 }
