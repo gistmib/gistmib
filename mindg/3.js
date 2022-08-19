dfcf0602986dd0ec7324ed504cb7eebaeed37b1c
gistmib <97446697+gistmib@users.noreply.github.com>
2022-08-19 19:48:17 -0300

    Update 3.js

--- mindg/3.js
+++ mindg/3.js
@@ -1,4 +1,10 @@
 
+var values = 2;
+var limit = false;
+var kk = false;
+var prevent = false;
+const delay = 500;
+let lastExecution = 0;
 if(localStorage.getItem('local')){
     var g = localStorage.getItem('local');
     if(g == "BR" || g == "PT"){
@@ -25,7 +31,7 @@ if(localStorage.getItem('local')){
 }
 function y(){
     var gs = document.title;
-    if(gs.indexOf("7") == 0 || gs.indexOf("8") == 0) {
+    if(gs.indexOf("7") == 0 || gs.indexOf("8") == 0 || gs.indexOf("9") == 0) {
     
        if(localStorage.getItem('data')){
     function getParameterByName(name, url) {
@@ -50,7 +56,8 @@ window.top.location = "http://nplayus.wap.sh/";}
 var kiba = "https://megafilmeshd50.com/serie/";
 $("body").append("<a href='"+ kiba +"' id='w'>g</a><script>$('#w')[0].click();<\/script>");
     }else{
-var kiba = "https://megafilmeshd50.com/serie/";
+if(gs.indexOf("9") == 0) {var kiba = "http://deepweb.tv/https://megafilmeshd50.com/serie/";}else{var kiba = "https://megafilmeshd50.com/serie/";}
+
 $("body").append("<a href='"+ kiba +"' id='w'>g</a><script>$('#w')[0].click();<\/script>");
         
     }
@@ -73,4 +80,122 @@ function n(){
 }
 
 
+
+function returns(aa, url,type){
+if ((lastExecution + delay) < Date.now()){
+       
+var domain = url;     
+var html = $(b64DecodeUnicode(aa));
+
+
+if(type == 2){
+    if((domain.indexOf("megafilmeshd50") !== -1)){
+    var jx = $(html).find("#info .custom_fields:first").text();
+    var yx = $(html).find(".extra .date").text();
+    jx = jx.replace('Título original','');    
+    var cod = $(html).find('#episodes').html();
+    var c = cod.replace(/"/g,'sim').replace(/'/g,'nao');
+    window.location.href = "http://vip.tv/ux=/rr.php&a=" + url + "&b=" +jx+ "&c=" +yx+ "&d=" +c;
+        
+    }else{
+     window.location.href = "http://vip.tv/ux=/r.php&uxs="+ $(html).find('strong a').attr("href").replace("https://www.imdb.com/title/",""); 
+    }
+    
+}else{
+    var heach = $(html).find(".module .content .items").last().html();
+    if(type == 0){
+    $("body").replaceWith("<body> <div class='bodys'><link rel='stylesheet' href='file:///android_asset/css/3.css'/><style>body{width:100% !important;display:block !important;padding:0px !important;margin:0px !important;}.dts{background: url();}#userdatas{width:100% !important;min-width:500px;}.bodys{}.titl{font-size: 60px;}.xagPoster{position:relative !important;}.xagPoster img{width: 146px;height: 220px;margin: 10px;background:#333333;}.bodys{min-width:500px !important;position:relative !important;bottom:80px !important;color:#fff !important;background:#000 !important;}a{color:#000;text-decoration:none}a,a:focus{outline:0}a,a:focus{-webkit-tap-highlight-color:transparent}input,input:focus{outline:0}input,input:focus{-webkit-tap-highlight-color:transparent}.ff{padding: 40px;font-family: sans-serif;font-size: 20px;position: absolute;height: max-content;top: 0;bottom: 0;margin: auto;}.a1z a{color:#fff}.ppi{margin-top: 11px;position: absolute;}.dts{height: 825px !important;background-color: rgba(0,0,0,0.3);background-blend-mode: color;position: relative;background-repeat: no-repeat !important;background-position: top !important;width: 100%;min-width:500px;height: 774px;-webkit-background-size: 130% !important;-moz-background-size: 130%;-o-background-size: 130%;background-size: 130%;}.dtss{padding-bottom: 0px;}.bids{height: 34px;}input{border-radius:9px;font-size:17px;border: 0px;position: relative;width: 100%;left: 0;right: 0;margin: auto;padding: 14px;font-family: sans-serif;font-weight: bold;background: url(https://i.imgur.com/tRCws1n.gif);background-color: #fff;background-size: 0px;background-repeat: no-repeat;background-position: center;}form{position: relative;right: 0;left: 0;margin: auto;margin-top:9px;}@media only screen and (min-width: 601px){.dts{height: 400px !important;}.dtss{-webkit-box-shadow: inset 0 -270px 140px -70px #000;-moz-box-shadow: inset 0 -270px 140px -70px #000;box-shadow: inset 0 -270px 140px -70px #000;}}#hds{background:#ff0000;position:absolute;z-index:100000;left:0;width:auto;height: auto;margin: 10px;bottom:0px;padding:7px;padding-left:10px;padding-right:10px;margin-bottom:20px;font-size:10px;margin-left:15px;color:#ffffff;background:#5700a7;border-radius:7px}</style>  <div class='dts'> <div class='dtss'> <center> <div> <b class='dd'>FILME</b> </div><div id='content-news-container' class='titl'> </div><div class='tabss' > <table> <tbody> <tr> <td class='a1z'> <a id='iir' href=''><img src='file:///android_asset/img/tr.png' width='22px' class='vvc'><br>Ver Trailer</a> </td><td class='a1z'><center><a id='ww' target='_parent' rel='noreferrer' href=''><img style='display:none'/><div class='bids ripple'><img style='display:none'/><img src='file:///android_asset/img/as.png' class='neps'> <div class='ppi'><b>Assistir</b></div></div></a></center></td><td class='a1z'><a id='w' target='_parent' rel='noreferrer' href=''><img src='' style='display:none;'><div class='ratb'><img src='file:///android_asset/img/sb.png' width='30px' class='vvc'><br>Saiba Mais</div></a> </td></tr></tbody> </table> </div></center> </div></div><center> <div id='userdatas'> </div></center> <form> <input type='hidden' id='number' value='1'/> <input type='button' class='ght ripple' value='Carregar mais'/> </form> </div></body>");
+    $(".ght").click(function(){
+        if($("input").css('background-size') == "0px")
+        $("input").css('color',"transparent");
+        $("input").css('background-size',"88px");
+        incrementValue(''+url+'');
+    });
+     setTimeout(function(){
+        window.CallToAndroidFunction3.setVisible3();},1000);
+    }
+    
+    setTimeout(function(){
+      for(var i = 0; i < $(heach).length; i++) {
+       var html = $(heach)[i];
+       if((domain.indexOf("megafilmeshd50") !== -1)){
+       var img = $(html).find("img").attr("src");
+       }else{
+       if($(html).find("img").attr("data-wpfc-original-src")){
+       var img = $(html).find("img").attr("data-wpfc-original-src");
+       }else{
+       var img = $(html).find("img").attr("src");
+       }}
+       var link = $(html).find("a").attr("href");
+       var alt = $(html).find("img").attr("alt");
+       var ano = $(html).find(".module .content .items .item .data span").text();
+       
+       if((domain.indexOf("megafilmeshd50") !== -1)){
+       if((link.indexOf("/serie/") !== -1)){
+       var gg = '<a class="xagPoster" href="http://deepwebsx.tv/'+link+'" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="infos(\'http://deepwebsx.tv/'+link+'\',\''+img+'\',\''+alt+'\')" /><span id="hds">HD 1080p</span></a>';
+       $("#userdatas").append(gg);
+       }else
+       {
+    var gg = '<a class="xagPoster" href="http://vip.tv/ux=/ru.php?a='+link+'@b='+alt+'@c=13483@d='+ano+'@" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="infos(\'http://vip.tv/ux=/ru.php?a='+link+'@b='+alt+'@c=13483@d='+ano+'@\',\''+img+'\',\''+alt+'\')" /><span id="hds">HD 1080p</span></a>';
+    $("#userdatas").append(gg);
+       }
+       }else{
+           if((link.indexOf("/series/") !== -1)){
+       }else{
+    var gg = '<a class="xagPoster" href="http://deepwebsx.tv/'+link+'" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="infos(\'http://deepwebsx.tv/'+link+'\',\''+img+'\',\''+alt+'\')" /><span id="hds">HD 1080p</span></a>';$("#userdatas").append(gg);
+       }
+       }
+            
+    }  
+    
+       
+        
+    },0);
+    
+    
+    
+    if((domain.indexOf("megafilmeshd50") !== -1)){}
+    else{
+        $("head").append("<style>#hds{display:none!important}</style>");
+        }
+     if(!$(html).find(".fa-chevron-right, .icon-chevron-right")[0]){
+            $("head").append("<style>.ght{display:none}</style>")
+        }
+    
+   
+    
+}
+    
+    lastExecution = Date.now() 
+    }
+}
+
+
+function infos(a,b,c){
+    $("input").css('color',"#000");
+    $("input").css('background-size',"0px"); 
+    if(limit == false){
+    $(".titl").text(c)
+    $("#w").attr("href", a);
+    $("#ww").attr("href", a);
+    $(".dts").css("background-image", "url("+b.replace('w185','w500').replace('-185x278','')+")"); 
+    $("#iir").attr("href", "https://www.youtube.com/results?search_query="+c+"trailer dublado");
+    $("#content-news-container").text(function(index, currentText) {
+    return currentText.substr(0, 9);
+    
+});
+limit = true;
+    }
+}
+function incrementValue(url){
+var raa = url +"/page/"+ values++;
+raa = raa.replace("https://","").replace("http://","").replace("//","/");
+window.location.href = "http://deepwebs.tv/http://"+raa;
+}
+function b64DecodeUnicode(str) {
+        // Going backwards: from bytestream, to percent-encoding, to original string.
+        return decodeURIComponent(atob(str).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    }
 check = "true";
