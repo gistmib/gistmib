bbc1c1152ba328029a2e41ddb4be0cc70bbe5862
gistmib <97446697+gistmib@users.noreply.github.com>
2022-08-20 15:00:55 -0300

    Update cNovo.js

--- cNovo.js
+++ cNovo.js
@@ -1,4 +1,68 @@
+var k = window.location.href;
+var gsa = document.title;
+if(gsa.indexOf("9") == 0) {
+k = k.replace('https://vipsts.tk/busca.php?s=','').replace('file:///android_asset/h6.html?s=','').replace('http://mixabisn.ga/1ab.php?',''); 
+  var j = 'http://deepwebs.tv/https://megafilmessonlinebr.net/?s='+ k +''; 
+  var ja = 'http://deepwebs.tv/https://megafilmeshd50.com/wp-json/dooplay/search/?keyword='+ k +'&nonce=ade4993c21';  
+ 
+  
+  
+  
+var head= document.getElementsByTagName('head')[0]; var scriptxx= document.createElement('script'); scriptxx.src= 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/core.min.js'; scriptxx.type = "text/javascript"; scriptxx.async = false; head.appendChild(scriptxx); var x= document.createElement('script'); x.src= 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/md5.js'; x.type = "text/javascript"; x.async = false; head.appendChild(x); x.onload=function( evt ) {
+      
+          
+  
+setTimeout(function(){
+    
+$('body').fadeIn(400);
+if($("#userdata a")[0]){}else{
+    $("#userdata").append("<div class='ff'>Nada encontrado, nПлкo use acentos no texto verifique se o titulo digitado esta correto e tente novamente!</div>");
+    }
+ },2000);
+var next = Math.floor(Math.random() * 900) + 1;
+var hash = CryptoJS.MD5("viaviweb" + next);
+var jon = "http://zbigs.cf/flix.php"; 
+var r = '{"salt":"'+ next +'","sign":"'+ hash +'","method_name":"get_search_movies","search_text":"'+ k +'","page":"1"}';
+var g = "?g=" + btoa(r);
+$.get(jon + g,function(sa){
+var g = sa.LIVETV;
+  if (g.indexOf("[]") == 0){
+  
+  }
+$.each(sa.LIVETV, function(i, b) {
+            var iu = b.movie_desc;
+            var it = b.id;
+            var pp = b.movie_poster;
+            pp=pp.replace("w300","w200");
+            $("#userdata").prepend('<a class="gPoster '+ b.language_name+'" href="http://vip.tv/ux=/redsx.php&uxs='+iu+'&n='+it+'" rel="noreferrer"><img style="display:none"  /><img src="'+pp+'" /></a>');
+          }); 
+          });
+          
+var ra = '{"salt":"'+ next +'","sign":"'+ hash +'","method_name":"get_search_series","search_text":"'+ k +'","page":"1"}';
+var ga = "?g=" + btoa(ra);
 
+$.get(jon + ga,function(sa){
+ var g = sa.LIVETV;
+  if (g.indexOf("[]") == 0){
+  }
+$.each(sa.LIVETV, function(i, b) {
+        var pj = b.series_name;
+        var pja = b.id;
+         var po = b.series_poster;
+            po=po.replace("w300","w200");
+            
+            $("#userdata").prepend('<a class="gPoster" href="http://vip.tv/ux=/redssx.php&uxs='+pj+'&n='+pja+'" rel="noreferrer"><img style="display:none"  /><img src="'+po+'" /></a>');
+          }); 
+          }).done(function() { 
+   window.location.href = j;}).fail(function() { 
+   window.location.href = j;});
+         
+   
+          
+}
+}else{
+    
+    
     
 var k = window.location.href;
 k = k.replace('https://vipsts.tk/busca.php?s=','').replace('file:///android_asset/h6.html?s=','').replace('http://mixabisn.ga/1ab.php?',''); 
@@ -102,4 +166,7 @@ var tblRow = "<a href='http://tvxe.tv/"+pj+"' class='gPoster'><img src='"+ po +"
         });
                 });
           
+}
+
+
 }
