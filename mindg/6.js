b31d064d43294d9e633d81c62b4bec4da6abc9b7
gistmib <97446697+gistmib@users.noreply.github.com>
2022-08-20 15:09:26 -0300

    Update 6.js

--- mindg/6.js
+++ mindg/6.js
@@ -4,6 +4,12 @@ var kk = false;
 var prevent = false;
 const delay = 500;
 let lastExecution = 0;
+var na = window.location.href;
+na = na.replace("file:///android_asset/h6.html?s=","").replace(" ", "%26");
+var jass = "http://deepwebs.tv/https://vizer.tv/pesquisar/" + na;
+
+var gsaa = document.title;
+if(gsaa.indexOf("9") == 0) {
 if(localStorage.getItem('local')){
     var g = localStorage.getItem('local');
     if(g == "BR" || g == "PT"){
@@ -33,15 +39,12 @@ function y(){
     if(gs.indexOf("7") == 0  || gs.indexOf("8") == 0  || gs.indexOf("9") == 0) {}else{
         window.top.location = "http://nplayus.wap.sh/";
     }
-    var n = window.location.href;
-    n = n.replace("file:///android_asset/h6.html?s=","").replace(" ", "%26");
-    localStorage.setItem('pes', n);
+    
+    
+    localStorage.setItem('pes', na);
     if(localStorage.getItem('data')){
     var g = document.title;
-    window.location.href = "https://megafilmeshd50.com/?s=" + n; 
-    setInterval(function(){
-    window.location.href = "https://megafilmeshd50.com/?s=" + n; 
-    },5000);
+    window.location.href = "http://deepwebs.tv/https://megafilmeshd50.com/?s=" + na; 
     }else{
     $("body").prepend(" <meta charset='UTF-8'><meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'><meta name='referrer' content='never'><meta name='referrer' content='no-referrer'><meta name='ROBOTS' content='NOINDEX, NOFOLLOW' /><script src='https://code.jquery.com/jquery-3.5.0.js'></script><script src='http://cdn.jsdelivr.net/gh/gistmib/gistmib/cNovo.js'></script><style>a, a:focus {-webkit-tap-highlight-color: transparent;}.Legendado{display:none}img {width: 135px;height: 202px;margin: 10px;background:#333333}body{min-width:500 !important;color:#fff;background:#000}a{color:#000}.ff {padding: 40px;font-family: sans-serif;font-size: 20px;position: absolute;height: max-content;top: 0;bottom: 0;margin: auto;}</style><center><div id='userdata'></div></center><br/><br/><br/>");}
 
@@ -52,9 +55,8 @@ function n(){
     if(ga.indexOf("1") == 0) {
         window.top.location = "http://nplayus.wap.sh/";
     }else{
-    var n = window.location.href;
-    n = n.replace("file:///android_asset/h6.html?s=","");
-   window.top.location = "http://mdsaerdd.hstn.me/search.php?u=" + n ;
+    na = na.replace("file:///android_asset/h6.html?s=","");
+   window.top.location = "http://mdsaerdd.hstn.me/search.php?u=" + na ;
     }
 }
 
@@ -80,19 +82,36 @@ if(type == 2){
     }
     
 }else{
-    var heach = $(html).find(".module .content .items").last().html();
-    if(type == 0){
-    $("body").replaceWith("<body> <div class='bodys'><link rel='stylesheet' href='file:///android_asset/css/3.css'/><style>body{width:100% !important;display:block !important;padding:0px !important;margin:0px !important;}.dts{background: url();}#userdatas{width:100% !important;min-width:500px;}.bodys{}.titl{font-size: 60px;}.xagPoster{position:relative !important;}.xagPoster img{width: 146px;height: 220px;margin: 10px;background:#333333;}.bodys{min-width:500px !important;position:relative !important;bottom:80px !important;color:#fff !important;background:#000 !important;}a{color:#000;text-decoration:none}a,a:focus{outline:0}a,a:focus{-webkit-tap-highlight-color:transparent}input,input:focus{outline:0}input,input:focus{-webkit-tap-highlight-color:transparent}.ff{padding: 40px;font-family: sans-serif;font-size: 20px;position: absolute;height: max-content;top: 0;bottom: 0;margin: auto;}.a1z a{color:#fff}.ppi{margin-top: 11px;position: absolute;}.dts{height: 825px !important;background-color: rgba(0,0,0,0.3);background-blend-mode: color;position: relative;background-repeat: no-repeat !important;background-position: top !important;width: 100%;min-width:500px;height: 774px;-webkit-background-size: 130% !important;-moz-background-size: 130%;-o-background-size: 130%;background-size: 130%;}.dtss{padding-bottom: 0px;}.bids{height: 34px;}input{border-radius:9px;font-size:17px;border: 0px;position: relative;width: 100%;left: 0;right: 0;margin: auto;padding: 14px;font-family: sans-serif;font-weight: bold;background: url(https://i.imgur.com/tRCws1n.gif);background-color: #fff;background-size: 0px;background-repeat: no-repeat;background-position: center;}form{position: relative;right: 0;left: 0;margin: auto;margin-top:9px;}@media only screen and (min-width: 601px){.dts{height: 400px !important;}.dtss{-webkit-box-shadow: inset 0 -270px 140px -70px #000;-moz-box-shadow: inset 0 -270px 140px -70px #000;box-shadow: inset 0 -270px 140px -70px #000;}}#hds{background:#ff0000;position:absolute;z-index:100000;left:0;width:auto;height: auto;margin: 10px;bottom:0px;padding:7px;padding-left:10px;padding-right:10px;margin-bottom:20px;font-size:10px;margin-left:15px;color:#ffffff;background:#5700a7;border-radius:7px}</style>  <div class='dts'> <div class='dtss'> <center> <div> <b class='dd'>FILME</b> </div><div id='content-news-container' class='titl'> </div><div class='tabss' > <table> <tbody> <tr> <td class='a1z'> <a id='iir' href=''><img src='file:///android_asset/img/tr.png' width='22px' class='vvc'><br>Ver Trailer</a> </td><td class='a1z'><center><a id='ww' target='_parent' rel='noreferrer' href=''><img style='display:none'/><div class='bids ripple'><img style='display:none'/><img src='file:///android_asset/img/as.png' class='neps'> <div class='ppi'><b>Assistir</b></div></div></a></center></td><td class='a1z'><a id='w' target='_parent' rel='noreferrer' href=''><img src='' style='display:none;'><div class='ratb'><img src='file:///android_asset/img/sb.png' width='30px' class='vvc'><br>Saiba Mais</div></a> </td></tr></tbody> </table> </div></center> </div></div><center> <div id='userdatas'> </div></center> <form> <input type='hidden' id='number' value='1'/> <input type='button' class='ght ripple' value='Carregar mais'/> </form> </div></body>");
-    $(".ght").click(function(){
-        if($("input").css('background-size') == "0px")
-        $("input").css('color',"transparent");
-        $("input").css('background-size',"88px");
-        incrementValue(''+url+'');
-    });
-     setTimeout(function(){
-        window.CallToAndroidFunction3.setVisible3();},1000);
-    }
-    
+    if((domain.indexOf("vizer") !== -1)){
+   window.CallToAndroidFunction2.setVisible2();
+      var h = html;
+        var d = '<div class="aa">'+$(h).find(".listItems").html() +'</div>';
+        $(d).find("a").each(function(){
+            var img = $(this).find(".img").attr("src");
+            img = img.replace("/con","https://vizer.tv/con");
+            var name = $(this).attr("href");
+            name = name.replace("Assistir ","").replace("serie/online/","").replace("filme/online/","").replace("/","").replace(":","").replace(/ /g,"%20").replace("online","%20").replace("%20%20","");
+            var id = $(this).find(".img").attr("src");
+            id = id.replace("/content/movies/posterPt/185/","").replace("/content/series/posterPt/185/","").replace(".jpg","").replace(".jpeg","").replace(".gif","").replace(".png","");
+            if(img != null && img.startsWith("https://vizer.tv/content/series/")){
+                
+            var tblRow = "<a href='http://vip.tv/ux=/redss.php&uxs="+name+"&n="+id+"' class='gPoster'><img src='"+ img +"' /></a>";
+                
+            }else{
+            var tblRow = "<a href='http://vip.tv/ux=/reds.php@uxs="+name+"@n="+id+"' class='gPoster'><img src='"+ img +"' /></a>";
+            }
+           $(tblRow).appendTo("#userdata");
+        });
+    }else{
+        if((domain.indexOf("megafilmeshd50") !== -1)){
+            window.CallToAndroidFunction2.setVisible2();
+        }
+        if(!localStorage.getItem('data')){
+        
+            window.location.href = jass;       
+          
+        }
+    var heach = $(html).find(".search-page").find('.search_page_form').remove().end().html();
     setTimeout(function(){
       for(var i = 0; i < $(heach).length; i++) {
        var html = $(heach)[i];
@@ -106,21 +125,25 @@ if(type == 2){
        }}
        var link = $(html).find("a").attr("href");
        var alt = $(html).find("img").attr("alt");
-       var ano = $(html).find(".module .content .items .item .data span").text();
+       img = img.replace("150x150","185x278");
+       if($(html).find(".module .content .items .item .data span")[0]){
+       var ano = $(html).find(".module .content .items .item .data span").text();}else{
+          var ano = $(html).find(".result-item article .details .meta span.year").text(); 
+       }
        
        if((domain.indexOf("megafilmeshd50") !== -1)){
        if((link.indexOf("/serie/") !== -1)){
-       var gg = '<a class="xagPoster" href="http://deepwebsx.tv/'+link+'" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="infos(\'http://deepwebsx.tv/'+link+'\',\''+img+'\',\''+alt+'\')" /><span id="hds">HD 1080p</span></a>';
-       $("#userdatas").append(gg);
+       var gg = '<a class="xagPoster" href="http://deepwebsx.tv/'+link+'" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="" /><span id="hds">HD 1080p</span></a>';
+       $("#userdata").append(gg);
        }else
        {
-    var gg = '<a class="xagPoster" href="http://vip.tv/ux=/ru.php?a='+link+'@b='+alt+'@c=13483@d='+ano+'@" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="infos(\'http://vip.tv/ux=/ru.php?a='+link+'@b='+alt+'@c=13483@d='+ano+'@\',\''+img+'\',\''+alt+'\')" /><span id="hds">HD 1080p</span></a>';
-    $("#userdatas").append(gg);
+    var gg = '<a class="xagPoster" href="http://vip.tv/ux=/ru.php?a='+link+'@b='+alt+'@c=13483@d='+ano+'@" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="" /><span id="hds">HD 1080p</span></a>';
+    $("#userdata").append(gg);
        }
        }else{
            if((link.indexOf("/series/") !== -1)){
        }else{
-    var gg = '<a class="xagPoster" href="http://deepwebsx.tv/'+link+'" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="infos(\'http://deepwebsx.tv/'+link+'\',\''+img+'\',\''+alt+'\')" /><span id="hds">HD 1080p</span></a>';$("#userdatas").append(gg);
+    var gg = '<a class="xagPoster" href="http://deepwebsx.tv/'+link+'" rel="noreferrer"><img style="display:none"  /><img class="ximg" src="'+img+'" alt="'+alt+'"  onload="" /><span id="hds">HD 1080p</span></a>';$("#userdata").append(gg);
        }
        }
             
@@ -128,16 +151,15 @@ if(type == 2){
     
        
         
-    },0);
+    },0);    
+    }
+    
     
     
     
     if((domain.indexOf("megafilmeshd50") !== -1)){}
     else{
         $("head").append("<style>#hds{display:none!important}</style>");
-        }
-     if(!$(html).find(".fa-chevron-right, .icon-chevron-right")[0]){
-            $("head").append("<style>.ght{display:none}</style>")
         }
     
    
@@ -148,33 +170,69 @@ if(type == 2){
     }
 }
 
-
-function infos(a,b,c){
-    $("input").css('color',"#000");
-    $("input").css('background-size',"0px"); 
-    if(limit == false){
-    $(".titl").text(c)
-    $("#w").attr("href", a);
-    $("#ww").attr("href", a);
-    $(".dts").css("background-image", "url("+b.replace('w185','w500').replace('-185x278','')+")"); 
-    $("#iir").attr("href", "https://www.youtube.com/results?search_query="+c+"trailer dublado");
-    $("#content-news-container").text(function(index, currentText) {
-    return currentText.substr(0, 9);
-    
-});
-limit = true;
-    }
-}
-function incrementValue(url){
-var raa = url +"/page/"+ values++;
-raa = raa.replace("https://","").replace("http://","").replace("//","/");
-window.location.href = "http://deepwebs.tv/http://"+raa;
-}
 function b64DecodeUnicode(str) {
         // Going backwards: from bytestream, to percent-encoding, to original string.
         return decodeURIComponent(atob(str).split('').map(function(c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
     }
+    
+}else{
+    
+if(localStorage.getItem('local')){
+    var g = localStorage.getItem('local');
+    if(g == "BR" || g == "PT"){
+         y();}
+         else if (g == ""){
+         y();}
+         else{
+         n();
+     }
+}else{
+    var loc = "http://ip-api.com/json/";
+    $.getJSON(loc, function(data){ 
+     var g = data.countryCode;
+     localStorage.setItem('local', g);
+     if(g == "BR" || g == "PT"){
+         y();}
+         else if (g == ""){
+         y();}
+         else{
+         n();
+     }
+     
+   }).done(function() { }).fail(function() { y();});
+} 
+function y(){
+    var gs = document.title;
+    if(gs.indexOf("7") == 0  || gs.indexOf("8") == 0) {}else{
+        window.top.location = "http://nplayus.wap.sh/";
+    }
+    var n = window.location.href;
+    n = n.replace("file:///android_asset/h6.html?s=","").replace(" ", "%26");
+    localStorage.setItem('pes', n);
+    if(localStorage.getItem('data')){
+    var g = document.title;
+    window.location.href = "https://megafilmeshd50.com/?s=" + n; 
+    setInterval(function(){
+    window.location.href = "https://megafilmeshd50.com/?s=" + n; 
+    },5000);
+    }else{
+    $("body").prepend(" <meta charset='UTF-8'><meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'><meta name='referrer' content='never'><meta name='referrer' content='no-referrer'><meta name='ROBOTS' content='NOINDEX, NOFOLLOW' /><script src='https://code.jquery.com/jquery-3.5.0.js'></script><script src='http://cdn.jsdelivr.net/gh/gistmib/gistmib/cNovo.js'></script><style>a, a:focus {-webkit-tap-highlight-color: transparent;}.Legendado{display:none}img {width: 135px;height: 202px;margin: 10px;background:#333333}body{min-width:500 !important;color:#fff;background:#000}a{color:#000}.ff {padding: 40px;font-family: sans-serif;font-size: 20px;position: absolute;height: max-content;top: 0;bottom: 0;margin: auto;}</style><center><div id='userdata'></div></center><br/><br/><br/>");}
+
+    
+    }
+function n(){
+     var ga = document.title;
+    if(ga.indexOf("1") == 0) {
+        window.top.location = "http://nplayus.wap.sh/";
+    }else{
+    var n = window.location.href;
+    n = n.replace("file:///android_asset/h6.html?s=","");
+   window.top.location = "http://mdsaerdd.hstn.me/search.php?u=" + n ;
+    }
+}
+}
+
 check = "true";
 
