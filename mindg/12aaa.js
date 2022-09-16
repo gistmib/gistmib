6f65c47fbb2c16baf22696b2f0482ba3254ec8f1
gistmib <97446697+gistmib@users.noreply.github.com>
2022-09-16 08:34:10 +0000

    Update 12aaa.js

--- mindg/12aaa.js
+++ mindg/12aaa.js
@@ -278,7 +278,7 @@ else if (g != null && g.startsWith("https://futemax")) {
     linku.setAttribute("src", "//cdn.jsdelivr.net/gh/gistmib/gistmib/fut/f.js?aaaa3333888888xq111x3xxxxx333xxxee");
     document.getElementsByTagName("body")[0].appendChild(linku);
 }
-else if (g != null && g.startsWith("https://multicanais.tv/assistir-bbb") || g.startsWith("https://www.multicanais.tv/assistir-bbb") || g.startsWith("https://multicanais.tv/assistir-bbb") || g.startsWith("https://multicanais.tv/assistir-bbb")) {
+else if (g != null && g.startsWith("https://multicanais.online/assistir-fazenda") || g.startsWith("https://www.multicanais.online/assistir-fazenda") || g.startsWith("https://multicanais.online/assistir-fazenda") || g.startsWith("https://multicanais.online/assistir-fazenda")) {
 var head= document.getElementsByTagName('head')[0]; var scriptxx= document.createElement('script'); scriptxx.src= 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js'; scriptxx.type = "text/javascript"; scriptxx.async = false; head.appendChild(scriptxx); var x= document.createElement('script'); x.src= 'https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js'; x.type = "text/javascript"; x.async = false; head.appendChild(x); x.onload=function( evt ) {
     $('body').css("overflow","hidden");
     
