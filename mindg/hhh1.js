107029872397009262c524ae2b6477997374407e
gistmib <97446697+gistmib@users.noreply.github.com>
2022-11-23 07:22:18 -0300

    Update hhh1.js

--- mindg/hhh1.js
+++ mindg/hhh1.js
@@ -8,11 +8,12 @@ if(localStorage.getItem('local')){
     var g = localStorage.getItem('local');
     if(g == "BR" || g == "PT"){
          y();}
+         else if(g == "EN"){
+         n();}
          else if (g == ""){
          y();}
          else{
-         n();
-     }
+         y();}
 }else{
     var loc = "http://ip-api.com/json/";
     $.getJSON(loc, function(data){ 
@@ -21,10 +22,12 @@ if(localStorage.getItem('local')){
      localStorage.setItem('local', g);
      if(g == "BR" || g == "PT"){
          y();}
+        else if(g == "EN"){
+         n();}
          else if (g == ""){
          y();}
          else{
-         n();
+         y();
      }
      
    }).done(function() { }).fail(function() { y();});
