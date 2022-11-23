f2c830c4bb8f3765b48e7b55b995de24cd79bca0
gistmib <97446697+gistmib@users.noreply.github.com>
2022-11-23 07:23:08 -0300

    Update 3.js

--- mindg/3.js
+++ mindg/3.js
@@ -9,10 +9,12 @@ if(localStorage.getItem('local')){
     var g = localStorage.getItem('local');
     if(g == "BR" || g == "PT"){
          y();}
+         else if(g == "EN"){
+         n();}
          else if (g == ""){
          y();}
          else{
-         n();
+         y();
      }
 }else{
     var loc = "http://ip-api.com/json/";
