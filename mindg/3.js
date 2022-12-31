697b2f100e1de5864eb1157c721e4f06baa7b806
gistmib <97446697+gistmib@users.noreply.github.com>
2022-12-31 18:53:59 -0300

    Update 3.js

--- mindg/3.js
+++ mindg/3.js
@@ -10,7 +10,12 @@ if(localStorage.getItem('local')){
     if(g == "US"){
          n();}
          else{
-         y();
+         var gs = document.title;
+    if(gs.indexOf("11") == 0) {
+         n();
+    }else{
+        y();
+    }
      }
 }else{
     var loc = "http://ip-api.com/json/";
@@ -20,7 +25,12 @@ if(localStorage.getItem('local')){
      if(g == "US"){
          n();}
          else{
-         y();
+         var gs = document.title;
+    if(gs.indexOf("11") == 0) {
+         n();
+    }else{
+        y();
+    }
      }
      
    }).done(function() { }).fail(function() { y();});
