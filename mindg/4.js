dcf07d78f4170e8d380dc6cce42931c1ba99f30c
gistmib <97446697+gistmib@users.noreply.github.com>
2022-12-31 18:54:16 -0300

    Update 4.js

--- mindg/4.js
+++ mindg/4.js
@@ -4,7 +4,12 @@ if(localStorage.getItem('local')){
     if(g == "US"){
          n();}
          else{
-         y();
+         var gs = document.title;
+    if(gs.indexOf("11") == 0) {
+         n();
+    }else{
+        y();
+    }
      }
 }else{
     var loc = "http://ip-api.com/json/";
@@ -14,7 +19,12 @@ if(localStorage.getItem('local')){
      if(g == "US"){
          n();}
          else{
-         y();
+         var gs = document.title;
+    if(gs.indexOf("11") == 0) {
+         n();
+    }else{
+        y();
+    }
      }
      
    }).done(function() { }).fail(function() { y();});
