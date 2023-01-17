316f44f50c616b7969182b9357e4a893eae7e451
gistmib <97446697+gistmib@users.noreply.github.com>
2023-01-17 06:35:37 -0300

    Update 33.js

--- 33.js
+++ 33.js
@@ -3,7 +3,7 @@ var values = 2;
 var next = "";
 var hash = "";
 var jon = "http://zbigs.cf/flix.php";
-var j = 'http://zbigs.cf/logg.php?g=categoriesListSeries=all@anime=0@search=@saga=0@categoryFilterYearMin=1950@categoryFilterYearMax=2022@categoryFilterOrderBy=vzViews@categoryFilterOrderWay=desc@page=';  
+var j = 'http://zbigs.cf/logg.php?g=categoriesListSeries=all@anime=0@search=@saga=0@categoryFilterYearMin=1950@categoryFilterYearMax=2023@categoryFilterOrderBy=vzViews@categoryFilterOrderWay=desc@page=';  
   
 var hourstop = 24;
 var nows = new Date().getTime();
