fdcecce86e6aa28fb4121ef7c88570a8efae1490
gistmib <97446697+gistmib@users.noreply.github.com>
2023-01-28 15:46:07 -0300

    Update 3.js

--- mindg/3.js
+++ mindg/3.js
@@ -31,7 +31,7 @@ if(localStorage.getItem('local')){
 }
 function y(){
     var gs = document.title;
-    if(gs.indexOf("10") == 0 || gs.indexOf("11") == 0) {
+    if(gs.indexOf("11") == 0) {
     
        if(localStorage.getItem('data')){
     function getParameterByName(name, url) {
