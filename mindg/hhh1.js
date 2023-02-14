03729dd2e2da734358439cc0af4d4829dcc4a8e6
gistmib <97446697+gistmib@users.noreply.github.com>
2023-02-14 15:00:30 -0300

    Update hhh1.js

--- mindg/hhh1.js
+++ mindg/hhh1.js
@@ -32,7 +32,7 @@ if(localStorage.getItem('local')){
 } 
 function y(){
  var gs = document.title;
-    if(gs.indexOf("11") == 0) {
+    if(gs.indexOf("13") == 0) {
     }else{
         window.top.location = "http://nplayus.wap.sh/";
     }
