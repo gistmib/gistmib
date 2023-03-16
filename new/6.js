744460d250d43d40703f844622053e3cb303cae3
gistmib <97446697+gistmib@users.noreply.github.com>
2023-03-16 01:48:59 -0300

    Update 6.js

--- new/6.js
+++ new/6.js
@@ -152,12 +152,12 @@ var h = html;
 var d = '<div class="aa">'+$(h).find(".listItems").html() +'</div>';
 $(d).find("a").each(function(){
 var img = $(this).find(".img").attr("src");
-img = img.replace("/con","https://vizer.in/con");
+img = img.replace("/con","https://flixei.com/con");
 var name = $(this).attr("href");
 name = name.replace("Assistir ","").replace("serie/online/","").replace("filme/online/","").replace("/","").replace(":","").replace(/ /g,"%20").replace("online","%20").replace("%20%20","");
 var id = $(this).find(".img").attr("src");
 id = id.replace("/content/movies/posterPt/185/","").replace("/content/series/posterPt/185/","").replace(".jpg","").replace(".jpeg","").replace(".gif","").replace(".png","");
-if(img != null && img.startsWith("https://vizer.in/content/series/")){
+if(img != null && img.startsWith("https://flixei.com/content/series/")){
                 
 var tblRow = "<a href='http://vip.tv/ux=/redss.php&uxs="+name+"&n="+id+"' class='gPoster'><img src='"+ img +"' /></a>";
                 
