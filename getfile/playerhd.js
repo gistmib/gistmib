434387f0d3f2a17f3e8f8576e4214ce2e9e937d2
gistmib <97446697+gistmib@users.noreply.github.com>
2023-04-07 22:22:18 -0300

    Create playerhd.js

---
+++ getfile/playerhd.js
@@ -0,0 +1,57 @@
+var local = window.location.href;
+var servers = [3,5,4,6];
+var i = 0;
+var head= document.getElementsByTagName('head')[0]; 
+var scriptxx= document.createElement('script'); 
+scriptxx.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
+scriptxx.type = "text/javascript";
+scriptxx.async = false;
+head.appendChild(scriptxx); 
+scriptxx.onload = function(event) {
+    var link = "https://playerhd.org/video/playerfteste.php?url=" + window.btoa(servers[i]+"/"+getParam("id")+"/none/none");
+    getFinalUrl(link);
+}
+
+function getFinalUrl(uris){
+    getHtml(uris, function(html){
+       var uri = getSecondPart(html, "window.location.href");
+       getHtml("https://zbigs.cf/flixs.php?u="+uri, function(html){
+           var uri = getSecondPart(html, "window.location.href");
+           if(uri == null){ 
+               i++;
+               var link = "https://playerhd.org/video/playerfteste.php?url=" + window.btoa(servers[i]+"/"+getParam("id")+"/none/none");
+               getFinalUrl(link);
+           }
+           else{
+              window.location.href = uri;
+           }
+       });
+   });
+}
+function getHtml(uri, onResult){
+    $.ajax({
+        url:uri,
+        type: 'GET',
+        success: function(data){
+            onResult(data);
+        },
+});
+}
+function getSecondPart(str, val) {
+    try {
+        var st = str.split(val)[1];
+        st = st.substring(0, st.indexOf('})')).replace(/"/g, "").replace(/ /g, "").replace(/=/g, "").replace("?link","?link=");
+        return st;
+    }
+    catch(e){
+        return null
+    }
+}
+function getParam(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
