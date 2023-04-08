abf2e5258148583ed7adeae5af4f0306b0f3708d
gistmib <97446697+gistmib@users.noreply.github.com>
2023-04-07 22:06:25 -0300

    Update sbb.js

--- streamsb/sbb.js
+++ streamsb/sbb.js
@@ -1,37 +1,38 @@
 var local = window.location.href;
-var servers = [3,5,4,6];
-var i = 0;
 var head= document.getElementsByTagName('head')[0]; 
-var scriptxx= document.createElement('script'); 
-scriptxx.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
-scriptxx.type = "text/javascript";
-scriptxx.async = false;
-head.appendChild(scriptxx); 
-scriptxx.onload = function(event) {
- if($('.g-recaptcha').length){
-                setTimeout(function(){$('button')[0].click();},10000);
-   }
-  else{
-  if($('.mb-4 > a').length) {
+var script= document.createElement('script'); 
+script.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
+script.type = "text/javascript";
+script.async = false;
+head.appendChild(script); 
+script.onload = function(event) {
+    if($('.mb-4 > a').length) {
         window.location.href = "http://videomega.tv/" +  $('.mb-4 > a').attr("href");
-   }
-   else{
-   var path = window.location.pathname.split("/").pop().replace(".html","").replace(".php","");
-                $.ajax({
-                    url:"/dl",
-                    type: 'POST',
-                    data:{op:"download_orig",id:path,mode:"n",hash:"0"},
-                    success: function(data){
-                        var html = data + "<script>setTimeout(function(){$('button')[0].click();},10000);<\/script>";
-                        $("body").html(html);
-                        if ("undefined" !== typeof history.pushState) {
-                            history.pushState("aa", "title", "/dl");
-                        } else {
-                            window.location.assign("/dl");
-                        }
-                },
-                
-            });
-      }
-  }
+    }
+    else{
+        var path = window.location.pathname.split("/").pop().replace(".html","").replace(".php","");
+        $.ajax({
+            url:"/dl",
+            type: 'POST',
+            data:{op:"download_orig",id:path,mode:"n",hash:"0"},
+            success: function(html){
+                $("body").html(html);
+                if ("undefined" !== typeof history.pushState) {
+                    history.pushState("aa", "title", "/dl");
+                } 
+                else {
+                    window.location.assign("/dl");
+                }
+            },
+        });
+    }
+    
+    $(document).bind('DOMNodeInserted', function(e) {
+        var iframe = document.querySelector('.grecaptcha-logo iframe');
+        if(iframe != null){
+            iframe.onload = function(){
+                $('button')[0].click();
+            };
+        }
+    });
 }
