a34a8ed5914e183319a6daf3152313709f946790
gistmib <97446697+gistmib@users.noreply.github.com>
2023-07-01 13:30:20 -0300

    Update mixdrop.js

--- getfile/mixdrop.js
+++ getfile/mixdrop.js
@@ -12,7 +12,7 @@ script.onload = function(event) {
   $('video').on('DOMSubtreeModified', function(){
     var url = $(this).attr("src");
     if(url !== "" && url !== null && url !== "undefined"){
-     window.location.href = "http://videomega.tv/https" + url;
+     window.location.href = "http://videomega.tv/https:" + url;
     }
   });
 }
