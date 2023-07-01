5135527a9b87cccb963435a45450eb279ae30058
gistmib <97446697+gistmib@users.noreply.github.com>
2023-07-01 13:28:34 -0300

    Update mixdrop.js

--- getfile/mixdrop.js
+++ getfile/mixdrop.js
@@ -1,1 +1,18 @@
-
+var local = window.location.href;
+var head= document.getElementsByTagName('head')[0]; 
+var script= document.createElement('script'); 
+script.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
+script.type = "text/javascript";
+script.async = false;
+head.appendChild(script); 
+script.onload = function(event) {
+  setTimeout(function(){
+    $(".vjs-big-play-button")[0].click();
+  },500); 
+  $('video').on('DOMSubtreeModified', function(){
+    var url = $(this).attr("src");
+    if(url !== "" && url !== null && url !== "undefined"){
+     window.location.href = "http://videomega.tv/https" + url;
+    }
+  });
+}
