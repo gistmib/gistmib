d01d25abb1203653398810eedf6359d62a607493
gistmib <97446697+gistmib@users.noreply.github.com>
2023-07-05 20:14:50 -0300

    Create mixdrops.js

---
+++ getfile/mixdrops.js
@@ -0,0 +1,18 @@
+var local = window.location.href;
+var head= document.getElementsByTagName('head')[0]; 
+var script= document.createElement('script'); 
+script.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
+script.type = "text/javascript";
+script.async = false;
+head.appendChild(script); 
+script.onload = function(event) {
+  setInterval(function(){
+    $(".vjs-big-play-button")[0].click();
+  },1500);
+  $('video').on('DOMSubtreeModified', function(){
+    var url = $(this).attr("src");
+    if(url !== "" && url !== null && url !== "undefined"){
+     window.location.href = "http://videomega.tv/https:" + url;
+    }
+  });
+}
