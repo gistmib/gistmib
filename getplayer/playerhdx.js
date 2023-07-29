f93b5ba715b6f8d82cfbf5575f63e8ed8bfc2c73
gistmib <97446697+gistmib@users.noreply.github.com>
2023-07-29 18:32:39 -0300

    aa

--- getplayer/playerhdx.js
+++ getplayer/playerhdx.js
@@ -21,7 +21,8 @@ script.onload = function(event) {
     $('button:contains("VF DUB"), button:contains("VF LEG")').remove();
     $('button:contains("BAIXAR")').remove();
 });
-    if($(".footer")[0]){$(".footer").remove();}
+    if($(".footer")[0]){
+        $(".footer").remove();}
     });
 }
 script.onerror = function(event) {
