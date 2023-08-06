e456c16bbda9201c0a71c2cb2fe80f9ba43bfda1
gistmib <97446697+gistmib@users.noreply.github.com>
2023-08-06 19:10:15 -0300

    Update synic_player.js

--- mindg/synic_player.js
+++ mindg/synic_player.js
@@ -1,7 +1,10 @@
-var url = window.location.href;
+document.querySelector("html").style.display = "none";
+document.querySelector("body").style.display = "none";
+
+var url = window.location.href; 
 var local = window.location.hostname;
 var locations = [
-    "streamtape strcloud strtapeadblock adblockstrtech streamtape.net streamtape.com", 
+    "streamtape strcloud strtapeadblock adblockstrtech streamtape.net streamtape.com",
     "megafilmeshd50.com megafilmeshd50.net",
     "playerhd.org",
     "diampokusy.com suzihaza.com vfilmesonline.net vanfem.com diasfem.com fembed.com",
@@ -30,7 +33,6 @@ function getLocation(uri, onResult){
 }
 
 function init(value){
-    //alert(value);
     if(value === "gdriveplayer.js"){
         window.location.href = "http://cdnplayer.tv/" + window.location.href;
     }else if(value === "voe.js"){
@@ -38,10 +40,12 @@ function init(value){
     }else{
         var head= document.getElementsByTagName('head')[0]; 
         var script= document.createElement('script'); 
-        script.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/getfile/' + value;
+        script.src= '//zbigz.in/9uhd/getfile/' + value + '?' + (Math.random() + 1).toString(36).substring(7);
         script.type = "text/javascript";
         script.async = false;
-        head.appendChild(script); 
+        head.appendChild(script);
+        script.onload(window.CallToAndroidFunction1.setVisible());
+        script.onerror(window.location.reload());
     }
 }
 function req(value){
@@ -54,7 +58,7 @@ function req(value){
         case 5: return "gdriveplayer.js";
         case 6: return "voe.js";
         case 7: return "mixdrop.js";
-        default: 
+        default:                  
         if(typeof(document.getElementById('vstr')) !== 'undefined' && document.getElementById('vstr') !== null) {
             return "fembed.js"; 
         }
