432b97f4963ee0db4991630f31208eddee3accb3
gistmib <97446697+gistmib@users.noreply.github.com>
2023-08-08 12:31:34 -0300

    Update mixdrop.js

--- getfile/mixdrop.js
+++ getfile/mixdrop.js
@@ -1,27 +1,39 @@
-var local = window.location.href;
-var head= document.getElementsByTagName('head')[0];
-var script= document.createElement('script'); 
-script.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
-script.type = "text/javascript";
-script.async = false;
-head.appendChild(script); 
-script.onload = function(event) {
-  if($(".error")[0]){
+var childLocation = window.location.href;
+var childHead = document.querySelector('head');
+var childScript = document.createElement('script'); 
+const interval = setInterval(function(){
+    var link = $("video").attr("src");
+    if(checkIsNullValue(link)){
+        window.location.href = `http://videomega.tv/https:${link}`;
+        clearInterval(interval);
+    }else{
+        $(".vjs-big-play-button")[0].click();
+    }
+}, 100);
+
+childScript.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
+childScript.type = "text/javascript";
+childScript.async = false;
+childScript.onload = () => init();
+childScript.onerror = () => fail();
+childHead.appendChild(childScript);
+
+function init() { 
+    if($(".error")[0] || !$(".vjs-big-play-button")[0]){
         window.location.href = "http://vip.tv/Erro";
-  }
-  else{
-      if($(".vjs-big-play-button")[0]){
-          setInterval(function(){ $(".vjs-big-play-button")[0].click(); },2000);
-      }else {
-          window.location.reload();
-      }
-    
-    
-    $('video').on('DOMSubtreeModified', function(){
-      var url = $(this).attr("src");
-      if(url !== "" && url !== null && url !== "undefined"){
-        window.location.href = "http://videomega.tv/https:" + url;
-      }
-    });
-  }
+    }
+    else{
+        interval;
+    }
+}
+function fail() {
+    window.location.reload();
+}
+function checkIsNullValue(value){
+    if(typeof(value)  === "undefined" || value === null || value === "" || typeof(value)  === "") {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
