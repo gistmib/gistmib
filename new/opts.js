07f0e1556ccc26b1a5110a99f18b9c9b82423a27
gistmib <97446697+gistmib@users.noreply.github.com>
2023-08-09 21:06:44 -0300

    aa

--- new/opts.js
+++ new/opts.js
@@ -130,7 +130,6 @@ function getParam(name, url) {
 }
 
 function programs(e){
-    alert("qq");
     var channel = getChannel(e);
     var valueSaved = getExpireValue(channel, 180);
     if(channel){
