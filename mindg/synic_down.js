f05ad703a60e5184fdd11884260da998c004d814
gistmib <97446697+gistmib@users.noreply.github.com>
2023-08-09 21:10:20 -0300

    aa

--- mindg/synic_down.js
+++ mindg/synic_down.js
@@ -1,69 +1,38 @@
 document.querySelector("html").style.display = "none";
 document.querySelector("body").style.display = "none";
 
+
 var head = document.querySelector('head');
 var script = document.createElement('script');
 var url = window.location.href; 
 var local = window.location.hostname;
-var locations = [
-    "streamtape strcloud strtapeadblock adblockstrtech streamtape.net streamtape.com",
-    "megafilmeshd50.com megafilmeshd50.net",
-    "playerhd.org",
-    "diampokusy.com suzihaza.com vfilmesonline.net vanfem.com diasfem.com fembed.com",
-    "streamsb.com sbface.com sbbrisk.com lvturbo.com",
-    "gdriveplayer.us gdriveplayer.to download.gdriveplayer.us",
-    "tubelessceliolymph.com voe.sx availedsmallest.com",
-    "mixdrop.co mixdrop mixdrop.com mixdrop.to"];
-
-
-
-function getLocation(uri){
+var objJson = [
+    {'search':'streamtape strcloud strtapeadblock adblockstrtech streamtape.net streamtape.com', 'value':'streamtape.js'},
+    {'search':'megafilmeshd50.com megafilmeshd50.net', 'value':'mfhd50.js'},
+    {'search':'playerhd.org', 'value':'playerhd.js'},
+    {'search':'mixdrop.co mixdrop mixdrop.com mixdrop.to', 'value':'mixdrop.js'}
+];
+function getLocationValue(local){
     let i = 0;
     do {
-        if(locations[i].search(uri) > -1){
-            return init(req(i));
+        if(objJson[i]['search'].search(local) > -1){
+            
+            return objJson[i]['value'];
         }
-        if(i == locations.length -1){
-            return "";
+        if(i == objJson.length -1){
+            if(typeof(document.getElementById('adb')) !== 'undefined' && document.getElementById('adb') !== null){
+                return "streamtape.js";
+            }
+            else if(typeof(document.getElementById('goUp')) !== 'undefined' && document.getElementById('goUp') !== null || typeof(document.getElementById('subfile')) !== 'undefined' && document.getElementById('subfile') !== null){
+                return "mixdrop.js";
+            }
+            else {
+                return "";
+            }
         }
         i++;
     }
-    while(i < locations.length);
-}
-
-function init(value){
-    return `//cdn.jsdelivr.net/gh/gistmib/gistmib@master/getfile/${value}`;
-}
-function req(value){
-    switch (value) {
-        case 0: return "streamtape.js";
-        case 1: return "mfhd50.js";
-        case 2: return "playerhd.js";
-        case 3: return "fembed.js";
-        case 4: return "streamsb.js";
-        case 5: return "gdriveplayer.js";
-        case 6: return "voe.js";
-        case 7: return "mixdrop.js";
-        default:                  
-        if(typeof(document.getElementById('vstr')) !== 'undefined' && document.getElementById('vstr') !== null) {
-            return "fembed.js"; 
-        }
-        else if(typeof(document.getElementById('adb')) !== 'undefined' && document.getElementById('adb') !== null){
-            return "streamtape.js";
-        }
-        else if(typeof(document.getElementsByClassName('modal-footer')[0]) !== 'undefined' && document.getElementsByClassName('modal-footer')[0] !== null || document.title === "StreamSB"){
-            return "streamsb.js";
-        }
-        else if(typeof(document.getElementById('voe-player')) !== 'undefined' && document.getElementById('voe-player') !== null){
-            return "voe.js";
-        }
-        else if(typeof(document.getElementById('goUp')) !== 'undefined' && document.getElementById('goUp') !== null || typeof(document.getElementById('subfile')) !== 'undefined' && document.getElementById('subfile') !== null){
-            return "mixdrop.js";
-        }
-        else {
-            return "";
-        }
-    }
+    while(i < objJson.length);
 }
 function onRequest(bol){
     if(bol){
@@ -88,11 +57,10 @@ function getCdn(value){
     return result;
 }
 
-script.src= getLocation(local);
+script.src= getCdn(getLocationValue(local));
 script.type = "text/javascript";
 script.onload = () => onRequest(true);
 script.onerror = () => onRequest(false);
 head.appendChild(script);
-
 
 check = "true";
