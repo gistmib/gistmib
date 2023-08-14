b444f3110756d89b4408d0ba11e4c316aa32acd7
gistmib <97446697+gistmib@users.noreply.github.com>
2023-08-13 22:35:40 -0300

    aa

--- getfile/mixdrop.js
+++ getfile/mixdrop.js
@@ -1,15 +1,7 @@
 var childLocation = window.location.href;
 var childHead = document.querySelector('head');
 var childScript = document.createElement('script'); 
-const interval = setInterval(function(){
-    var link = $("video").attr("src");
-    if(checkIsNullValue(link)){
-        window.location.href = `http://videomega.tv/https:${link}`;
-        clearInterval(interval);
-    }else{
-        $(".vjs-big-play-button")[0].click();
-    }
-}, 1000);
+var mInterval;
 
 childScript.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
 childScript.type = "text/javascript";
@@ -19,12 +11,22 @@ childScript.onerror = () => fail();
 childHead.appendChild(childScript);
 
 function init() { 
-    if($(".error")[0] || !$(".vjs-big-play-button")[0]){
-        window.location.href = "http://vip.tv/Erro";
+    $(document).ready(() => {
+        if($(".error")[0] || !$(".vjs-big-play-button")[0]){
+            window.location.href = "http://vip.tv/Erro";
+        }
+        else{
+            mInterval = setInterval(function(){
+                var link = $("video").attr("src");
+                if(checkIsNullValue(link)){
+                    window.location.href = `http://videomega.tv/https:${link}`;
+                    clearInterval(mInterval);
+                }else{
+                    $(".vjs-big-play-button")[0].click();
+                }
+            }, 1000);
     }
-    else{
-        interval;
-    }
+    });
 }
 function fail() {
     window.location.reload();
