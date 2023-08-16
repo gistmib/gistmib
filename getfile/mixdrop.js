1e5775040b1260c38c61fe7b612bcccce9a5e0b9
gistmib <97446697+gistmib@users.noreply.github.com>
2023-08-15 22:47:15 -0300

    ww

--- getfile/mixdrop.js
+++ getfile/mixdrop.js
@@ -3,6 +3,7 @@ var childHead = document.querySelector('head');
 var childScript = document.createElement('script'); 
 var mInterval;
 
+
 childScript.src= '//cdn.jsdelivr.net/gh/gistmib/gistmib@master/jquery.js';
 childScript.type = "text/javascript";
 childScript.async = false;
@@ -12,17 +13,27 @@ childHead.appendChild(childScript);
 
 function init() { 
     $(document).ready(() => {
-        if($(".error")[0] || !$(".vjs-big-play-button")[0]){
+        if($(".error")[0]){
             window.location.href = "http://vip.tv/Erro";
         }
         else{
             mInterval = setInterval(function(){
-                var link = $("video").attr("src");
-                if(checkIsNullValue(link)){
-                    window.location.href = `http://videomega.tv/https:${link}`;
-                    clearInterval(mInterval);
-                }else{
-                    $(".vjs-big-play-button")[0].click();
+                var video = $("video");
+                if(video[0]){
+                    var link = video.attr("src");
+                    if(checkIsNullValue(link)){
+                        window.location.href = `http://videomega.tv/https:${link}`;
+                        clearInterval(mInterval);
+                    }else{
+                        try{
+                            $(".vjs-big-play-button")[0].click();
+                        }
+                        catch(err){
+                            fail();
+                        }
+                    }
+                }else {
+                    fail();
                 }
             }, 1000);
     }
