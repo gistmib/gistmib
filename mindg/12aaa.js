9c170537505ee5e316a2ff06812a423014c0dd10
gistmib <97446697+gistmib@users.noreply.github.com>
2023-10-06 20:45:32 -0300

    Update 12aaa.js

--- mindg/12aaa.js
+++ mindg/12aaa.js
@@ -3,7 +3,7 @@ const parentHead = document.querySelector('head');
 const parentScript = document.createElement('script');
 const parentUrl = window.location.href;
 const parentLocal = window.location.hostname;
-const parentIsValidLink = ('https:' === location.protocol || 'htttp:' === location.protocol) ? true : false;
+const parentIsValidLink = ('https:' === location.protocol || 'http:' === location.protocol) ? true : false;
 const parentProtocol = parentIsValidLink ? location.protocol : 'https:';
 const parentObjJson = [
     {'search':'multicanais multicanais.vc multicanais.fans bbbgratis.com', 'value':'initMultiCanais()'},
